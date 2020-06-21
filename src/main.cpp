834474264f21f0418bc4db2c4e069c49a7521cc6
deltaphi <damian.philipp@gmx.net>
2020-06-21 11:56:26 +0200

    Implemented Blink example.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+constexpr static const uint8_t kLedPin = 13;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(kLedPin, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
+  digitalWrite(kLedPin, HIGH);
+  delay(1000);
+  digitalWrite(kLedPin, LOW);
+  delay(1000);}
